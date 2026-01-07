f926f02ff34d1d0cc2434b429c6f27a61953a022
TaeHUN <tehon79@gmail.com>
2026-01-07 15:08:30 +0900

    feat: Dto 수정 및 추가

--- src/main/java/com/example/monew/domain/comment/dto/CommentUpdateRequest.java
+++ src/main/java/com/example/monew/domain/comment/dto/CommentUpdateRequest.java
@@ -1,20 +1,14 @@
 package com.example.monew.domain.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CommentCreateRequest {
-
-    @NotNull
-    private UUID articleId;
+public class CommentUpdateRequest {
 
     @NotBlank
     private String content;
