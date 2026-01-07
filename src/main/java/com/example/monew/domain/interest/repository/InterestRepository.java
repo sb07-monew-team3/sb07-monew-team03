50cdc70853d44de1e88cd68b34aa5875425a6479
atto-jw <atto.jw@gmail.com>
2026-01-07 16:00:35 +0900

    feat : 관심사 및 키워드 레포지토리 추가

--- src/main/java/com/example/monew/domain/interest/repository/InterestRepository.java
+++ src/main/java/com/example/monew/domain/interest/repository/InterestRepository.java
@@ -1,4 +1,13 @@
 package com.example.monew.domain.interest.repository;
 
-public interface InterestRepository {
+import com.example.monew.domain.interest.entity.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface InterestRepository extends JpaRepository<Interest, UUID> {
+
+    Optional<Interest> findByName(String name);
+    boolean existsByName(String name);
 }
---
+++ src/main/java/com/example/monew/domain/interest/repository/KeywordRepository.java
@@ -0,0 +1,7 @@
+package com.example.monew.domain.interest.repository;
+
+import com.example.monew.domain.interest.entity.Keyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+}
