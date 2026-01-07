96509a06dd6c1a17e9e444922ca427bd4e9ad371
atto-jw <atto.jw@gmail.com>
2026-01-07 16:02:25 +0900

    refactor: 관심사 등록 예외 처리 로직 추가

--- src/main/java/com/example/monew/domain/interest/service/InterestService.java
+++ src/main/java/com/example/monew/domain/interest/service/InterestService.java
@@ -22,6 +22,14 @@ public class InterestService {
 
     public InterestDto create(InterestRegisterRequest request) {
 
+        if (request.name() == null || request.name().isBlank()) {
+            throw new IllegalArgumentException("관심사 이름은 필수입니다.");
+        }
+
+        if(interestRepository.existsByName(request.name())) {
+            throw new IllegalArgumentException("이미 존재하는 관심사입니다.");
+        }
+
         Interest interest = new Interest(request.name());
         Interest saved = interestRepository.save(interest);
 
