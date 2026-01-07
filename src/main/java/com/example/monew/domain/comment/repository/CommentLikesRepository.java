77d0ea1fd82725155f5cff6b02652c29037a5a68
TaeHUN <tehon79@gmail.com>
2026-01-07 15:11:22 +0900

    refactor: 레포지토리 수정

--- src/main/java/com/example/monew/domain/comment/repository/CommentLikesRepository.java
+++ src/main/java/com/example/monew/domain/comment/repository/CommentLikesRepository.java
@@ -3,14 +3,11 @@ package com.example.monew.domain.comment.repository;
 import com.example.monew.domain.comment.entity.CommentLikes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface CommentLikesRepository extends JpaRepository<CommentLikes, UUID> {
 
     boolean existsByUser_IdAndComment_Id(UUID userId, UUID commentId);
-
-    Optional<CommentLikes> findByUser_IdAndComment_Id(UUID userId, UUID commentId);
 
     long countByComment_Id(UUID commentId);
 
