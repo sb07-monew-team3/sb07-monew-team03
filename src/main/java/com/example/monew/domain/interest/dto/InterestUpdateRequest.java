085742a1976dadca89c039ef1ec55234dc28a904
atto-jw <atto.jw@gmail.com>
2026-01-08 18:28:04 +0900

    refactor: 관심사 수정 요청 DTO @NotEmpty 애노테이션 추가

--- src/main/java/com/example/monew/domain/interest/dto/InterestUpdateRequest.java
+++ src/main/java/com/example/monew/domain/interest/dto/InterestUpdateRequest.java
@@ -1,9 +1,12 @@
 package com.example.monew.domain.interest.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
 public record InterestUpdateRequest(
 
+        @NotEmpty
         List<String> keywords
 )
 {}
