2167f69cf88a89ff7986a8d8e22dc8d1debcd104
atto-jw <atto.jw@gmail.com>
2026-01-12 15:31:26 +0900

    refactor: 관심사 서비스 수정 및 구독 관련 로직 분리
    - 구독 서비스 관련 메서드 제거
    - 관심사 검색 메서드에 구독 상태 로직 추가
    - 구독자 수 계산 로직 추가

--- src/main/java/com/example/monew/domain/interest/service/InterestServiceImpl.java
+++ src/main/java/com/example/monew/domain/interest/service/InterestServiceImpl.java
@@ -5,14 +5,11 @@ import com.example.monew.domain.interest.dto.InterestRegisterRequest;
 import com.example.monew.domain.interest.dto.InterestUpdateRequest;
 import com.example.monew.domain.interest.entity.Interest;
 import com.example.monew.domain.interest.entity.Keyword;
-import com.example.monew.domain.interest.entity.Subscription;
 import com.example.monew.domain.interest.mapper.InterestMapper;
 import com.example.monew.domain.interest.repository.InterestRepository;
 import com.example.monew.domain.interest.repository.KeywordRepository;
 import com.example.monew.domain.interest.repository.SubscriptionRepository;
-import com.example.monew.domain.user.entity.User;
 import com.example.monew.domain.user.repository.UserRepository;
-import com.example.monew.global.exception.domain.user.UserNotExistException;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.springframework.http.HttpStatus;
@@ -67,7 +64,9 @@ public class InterestServiceImpl implements InterestService {
 
         keywordRepository.saveAll(keywords);
 
-        return interestMapper.toDto(interest, request.keywords());
+        Long count = subscriptionRepository.countByInterestId(interestId);
+
+        return interestMapper.toDto(interest, request.keywords(), count, null);
     }
 
     @Override
@@ -80,7 +79,7 @@ public class InterestServiceImpl implements InterestService {
 
     @Override
     @Transactional(readOnly = true)
-    public List<InterestDto> search(String keyword) {
+    public List<InterestDto> search(String keyword, UUID userId) {
         List<Interest> interests = interestRepository.searchByInterestOrKeyword(keyword);
 
         List<InterestDto> result = new ArrayList<>();
@@ -92,33 +91,14 @@ public class InterestServiceImpl implements InterestService {
                     .map(key -> key.getKeyword())
                     .toList();
 
-            InterestDto dto = interestMapper.toDto(interest, keywords);
+            Long count = subscriptionRepository.countByInterestId(interest.getId());
+            boolean subscribed = subscriptionRepository.isSubscribed(userId, interest.getId());
+
+            InterestDto dto = interestMapper.toDto(interest, keywords, count, subscribed);
 
             result.add(dto);
         }
         return result;
-    }
-
-    @Override
-    public void subscribe(UUID userId, UUID interestId) {
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotExistException(userId));
-
-        Interest interest = interestRepository.findById(interestId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "관심사가 없습니다."));
-
-        Subscription subscription = new Subscription(interest, user);
-
-        subscriptionRepository.save(subscription);
-    }
-
-    @Override
-    public void unsubscribe(UUID userId, UUID interestId) {
-        Subscription subscription = subscriptionRepository.findSubscription(userId, interestId)
-                .orElseThrow(() -> new IllegalArgumentException("구독이 없습니다."));
-
-        subscriptionRepository.delete(subscription);
     }
 
     private void validInterestName(String name) {
