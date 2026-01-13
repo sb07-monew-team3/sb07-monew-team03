c5a7eec44c4c782d5e8c2f93379db9511fbac011
atto-jw <atto.jw@gmail.com>
2026-01-13 14:05:03 +0900

    feat: 관심사 검색 커스텀 리포지토리 추가
    - 검색, 정렬, 페이징 로직 구현

--- src/main/java/com/example/monew/domain/interest/repository/InterestRepository.java
+++ src/main/java/com/example/monew/domain/interest/repository/InterestRepository.java
@@ -3,23 +3,14 @@ package com.example.monew.domain.interest.repository;
 import com.example.monew.domain.interest.entity.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface InterestRepository extends JpaRepository<Interest, UUID> {
-
-    Optional<Interest> findByName(String name);
+public interface InterestRepository extends JpaRepository<Interest, UUID>, InterestRepositoryCustom {
 
     boolean existsByName(String name);
 
     @Query("SELECT i.name FROM Interest i")
     List<String> findAllNames();
-
-    @Query("SELECT i FROM Interest i " +
-            "LEFT JOIN Keyword k ON k.interest = i " +
-            "WHERE i.name LIKE %:keyword% OR k.keyword LIKE %:keyword%")
-    List<Interest> searchByInterestOrKeyword(@Param("keyword") String keyword);
 }
