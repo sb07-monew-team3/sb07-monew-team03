0792dbde6d054ad98d144035e4d3939b34ec1c03
OfficialHwempire <genius5375@gmail.com>
2026-01-14 11:10:23 +0900

    feature: 테스트 할 때 scheduler 돌아가지 않게 설정할 수 있도록 enableproperty 추가 + naver client_id 없으면 null 값 받도록 추가

--- src/test/java/com/example/monew/domain/user/util/TestFixture.java
+++ src/test/java/com/example/monew/domain/user/util/TestFixture.java
@@ -1,6 +1,10 @@
 package com.example.monew.domain.user.util;
 
 
+import com.example.monew.domain.article.entity.Article;
+import com.example.monew.domain.comment.dto.CommentCreateRequest;
+import com.example.monew.domain.interest.dto.InterestRegisterRequest;
+import com.example.monew.domain.interest.entity.Interest;
 import com.example.monew.domain.user.dto.UserDto;
 import com.example.monew.domain.user.dto.UserLoginRequest;
 import com.example.monew.domain.user.dto.UserRegisterRequest;
@@ -9,6 +13,11 @@ import com.example.monew.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.media.Content;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Component
@@ -31,7 +40,32 @@ public class TestFixture {
         return new UserUpdateRequest(randomString());
     }
 
+    public CommentCreateRequest commentCreateRequestFactory(UUID articleId,UUID userId){
+        return new CommentCreateRequest(
+                articleId,
+                userId,
+                randomString()
+                );
+    }
+    public Article articleFactory(List<Interest> interests){
+        return new Article(
+                randomString(),
+                randomString(),
+                randomString(),
+                LocalDateTime.now(),
+                randomString(),
+                false,
+                Instant.now().minus(Duration.ofMinutes( new Random().nextInt(100))),
+                interests
+        );
+    }
 
+    public InterestRegisterRequest interestRegisterRequestFactory(){
+        return new InterestRegisterRequest(
+                randomString(),
+                List.of(randomString(),randomString(),randomString())
+        );
+    }
 
 
     String randomString(){
