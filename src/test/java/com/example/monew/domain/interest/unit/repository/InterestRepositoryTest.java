fe1b66e896f502ea8110c49d0ec4462b79ddaac0
atto-jw <atto.jw@gmail.com>
2026-01-16 15:27:16 +0900

    test: 관심사 레포지토리 테스트 추가 (정렬 및 구독자수 기준)

--- src/test/java/com/example/monew/domain/interest/unit/repository/InterestRepositoryTest.java
+++ src/test/java/com/example/monew/domain/interest/unit/repository/InterestRepositoryTest.java
@@ -2,11 +2,16 @@ package com.example.monew.domain.interest.unit.repository;
 
 import com.example.monew.domain.interest.entity.Interest;
 import com.example.monew.domain.interest.entity.Keyword;
+import com.example.monew.domain.interest.entity.Subscription;
 import com.example.monew.domain.interest.repository.InterestRepository;
 import com.example.monew.domain.interest.repository.KeywordRepository;
+import com.example.monew.domain.interest.repository.SubscriptionRepository;
+import com.example.monew.domain.user.entity.User;
+import com.example.monew.domain.user.repository.UserRepository;
 import com.example.monew.global.config.JpaAuditingConfig;
 import com.example.monew.global.config.QueryDslConfig;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,10 +39,24 @@ public class InterestRepositoryTest {
     private KeywordRepository keywordRepository;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+    @Autowired
     private EntityManager em;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
+
+        subscriptionRepository.deleteAll();
+        keywordRepository.deleteAll();
+        interestRepository.deleteAll();
+
+        em.flush();
+        em.clear();
+
         Interest interest = new Interest("코딩");
         Interest interest2 = new Interest("하드웨어");
         Interest interest3 = new Interest("금융");
@@ -52,13 +71,23 @@ public class InterestRepositoryTest {
                 new Keyword("비트코인", interest3),
                 new Keyword("코스닥", interest3)
         );
-
         keywordRepository.saveAll(keywords);
 
-        em.flush();
-        em.clear();
+        User user1 = new User("a@a.com", "테스트1", "1234", null);
+        User user2 = new User("b@b.com", "테스트2", "1234", null);
+        User user3 = new User("c@c.com", "테스트3", "1234", null);
+        userRepository.saveAll(List.of(user1, user2, user3));
+
+        Subscription sub1 = new Subscription(interest, user1);
+        Subscription sub2 = new Subscription(interest, user2);
+        Subscription sub3 = new Subscription(interest, user3);
+        Subscription sub4 = new Subscription(interest2, user1);
+        Subscription sub5 = new Subscription(interest2, user2);
+        Subscription sub6 = new Subscription(interest3, user1);
+        subscriptionRepository.saveAll(List.of(sub1, sub2, sub3, sub4, sub5, sub6));
+
     }
-    
+
     @Test
     @DisplayName("관심사 이름으로 검색할 수 있다")
     void search_interestName_Success() {
@@ -98,8 +127,50 @@ public class InterestRepositoryTest {
         assertThat(search).hasSize(1);
         assertThat(search.get(0).getName()).isEqualTo("코딩");
     }
+    
+    @Test
+    @DisplayName("이름을 내림차순으로 정렬한다")
+    void search_sortByName_desc() {
+
+        // given
+        String keyword = null; //전체 조회하기
+        String orderBy = "name";
+        String direction = "desc";
+        String cursor = null;
+        Instant after = null;
+        int limit = 10;
+
+        // when
+        List<Interest> search = interestRepository.searchByInterestOrKeyword(keyword, orderBy, direction, cursor, after, limit);
 
 
+        // then
+        assertThat(search).hasSize(3);
+        assertThat(search).extracting("name")
+                .containsExactly("하드웨어", "코딩", "금융");
 
+    }
+    
+    @Test
+    @DisplayName("구독자수 오름차순으로 정렬한다")
+    void search_sortBySubscriptionCount_asc() {
+
+        // given
+        String keyword = null; //전체 조회하기
+        String orderBy = "subscriberCount";
+        String direction = "asc";
+        String cursor = null;
+        Instant after = null;
+        int limit = 10;
+        
+        // when
+        List<Interest> search = interestRepository.searchByInterestOrKeyword(keyword, orderBy, direction, cursor, after, limit);
+        
+        
+        // then
+        assertThat(search).hasSize(3);
+        assertThat(search).extracting("name")
+                .containsExactly("금융", "하드웨어", "코딩");
+    }
 
 }
