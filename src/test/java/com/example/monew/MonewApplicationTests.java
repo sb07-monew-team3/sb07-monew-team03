43a591b4e3bff095b07c306c36ebac8b0d8611ec
OfficialHwempire <genius5375@gmail.com>
2026-01-19 13:59:40 +0900

    feat: 그냥 깃 액션 컴퓨터에 postgresql 하나 깔기

--- src/test/java/com/example/monew/MonewApplicationTests.java
+++ src/test/java/com/example/monew/MonewApplicationTests.java
@@ -2,16 +2,9 @@ package com.example.monew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
 class MonewApplicationTests {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
 
     @Test
     void contextLoads() {
