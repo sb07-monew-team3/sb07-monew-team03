52cabfb7f385af6fdef562c49596ffa25894159d
OfficialHwempire <genius5375@gmail.com>
2026-01-19 13:48:13 +0900

    feat: testcontainer 의존성 추가 및 monewapplicatioin test 실행시 testcontainer docker 뛰우도록 함

--- src/test/java/com/example/monew/MonewApplicationTests.java
+++ src/test/java/com/example/monew/MonewApplicationTests.java
@@ -2,9 +2,16 @@ package com.example.monew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class MonewApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
 
     @Test
     void contextLoads() {
