74ea042b6d4b4e2f2b9085e2c79c6a802776da6d
atto-jw <atto.jw@gmail.com>
2026-01-19 14:58:02 +0900

    refactor: 관심사 정렬 및 커서 처리 로직 메서드 분리

--- src/main/java/com/example/monew/domain/interest/repository/InterestRepositoryImpl.java
+++ src/main/java/com/example/monew/domain/interest/repository/InterestRepositoryImpl.java
@@ -4,6 +4,8 @@ import com.example.monew.domain.interest.entity.Interest;
 import com.example.monew.domain.interest.entity.QInterest;
 import com.example.monew.domain.interest.entity.QKeyword;
 import com.example.monew.domain.interest.entity.QSubscription;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -29,72 +31,72 @@ public class InterestRepositoryImpl implements InterestRepositoryCustom {
         QKeyword qKeyword = QKeyword.keyword1;
         QSubscription qSubscription = QSubscription.subscription;
 
-        JPAQuery<Interest> query = jpaQueryFactory
+        return jpaQueryFactory
                 .selectFrom(qInterest)
-                .leftJoin(qKeyword)
-                .on(qKeyword.interest.id.eq(qInterest.id))
-                .leftJoin(qSubscription)
-                .on(qSubscription.interest.id.eq(qInterest.id));
+                .leftJoin(qKeyword).on(qKeyword.interest.id.eq(qInterest.id))
+                .leftJoin(qSubscription).on(qSubscription.interest.id.eq(qInterest.id))
+                .where(
+                        containsKeyword(keyword),
+                        nameCursor(orderBy, direction, cursor, after))
+                .groupBy(qInterest.id)
+                .having(subscriberCountCursor(orderBy, direction, cursor, after))
+                .orderBy(orderCondition(orderBy, direction))
+                .limit(limit + 1)
+                .fetch();
 
-        // 부분일치
-        if (keyword != null && !keyword.isBlank()) {
-            query.where(
-                    qInterest.name.containsIgnoreCase(keyword)
-                            .or(qKeyword.keyword.containsIgnoreCase(keyword))
-            );
-        }
-        query.groupBy(qInterest.id); // 중복 없이 count 계산?
+    }
 
-        //커서
-        if (cursor != null && !cursor.isBlank() && after != null) {
-            if ("name".equals(orderBy)) {
-                if ("ASC".equals(direction)) {
-                    query.where(
-                            qInterest.name.gt(cursor)
-                                    .or(qInterest.name.eq(cursor)
-                                            .and(qInterest.createdAt.gt(after)))
-                    );
-                } else {
-                    query.where(
-                            qInterest.name.lt(cursor)
-                                    .or(qInterest.name.eq(cursor)
-                                            .and(qInterest.createdAt.lt(after)))
-                    );
-                }
-            } else if ("subscriberCount".equals(orderBy)) {
-                Long cursorCount = Long.parseLong(cursor);
-                if ("ASC".equals(direction)) {
-                    query.having(
-                            qSubscription.id.count().gt(cursorCount)
-                                    .or(qSubscription.id.count().eq(cursorCount)
-                                            .and(qInterest.createdAt.gt(after)))
-                    );
-                } else {
-                    query.having(
-                            qSubscription.id.count().lt(cursorCount)
-                                    .or(qSubscription.id.count().eq(cursorCount)
-                                            .and(qInterest.createdAt.lt(after)))
-                    );
-                }
-            }
-        }
+    // 부분 일치
+    private BooleanExpression containsKeyword(String keyword) {
+        if (keyword == null || keyword.isBlank()) return null;
 
-        // 정렬
+        return QInterest.interest.name.containsIgnoreCase(keyword)
+                .or(QKeyword.keyword1.keyword.containsIgnoreCase(keyword));
+    }
+
+    // 이름 커서
+    private BooleanExpression nameCursor(String orderBy, String direction, String cursor, Instant after) {
+        if (!"name".equals(orderBy) || cursor == null || after == null) return null;
+
+        QInterest qInterest = QInterest.interest;
+
+        return "ASC".equalsIgnoreCase(direction)
+                ? qInterest.name.gt(cursor).or(qInterest.name.eq(cursor).and(qInterest.createdAt.gt(after)))
+                : qInterest.name.lt(cursor).or(qInterest.name.eq(cursor).and(qInterest.createdAt.lt(after)));
+    }
+
+    // 구독자수 커서
+    private BooleanExpression subscriberCountCursor(String orderBy, String direction, String cursor, Instant after) {
+        if (!"subscriberCount".equals(orderBy) || cursor == null || after == null) return null;
+
+        long cursorCount = Long.parseLong(cursor);
+        QInterest qInterest = QInterest.interest;
+        QSubscription qSubscription = QSubscription.subscription;
+
+        return "ASC".equalsIgnoreCase(direction)
+                ? qSubscription.id.countDistinct().gt(cursorCount)
+                .or(qSubscription.id.countDistinct().eq(cursorCount).and(qInterest.createdAt.gt(after)))
+                : qSubscription.id.countDistinct().lt(cursorCount)
+                .or(qSubscription.id.countDistinct().eq(cursorCount).and(qInterest.createdAt.lt(after)));
+    }
+
+    // 정렬
+    private OrderSpecifier<?>[] orderCondition(String orderBy, String direction) {
+
+        QInterest qInterest = QInterest.interest;
+        QSubscription qSubscription = QSubscription.subscription;
+        boolean isAsc = "ASC".equalsIgnoreCase(direction);
+
         if ("name".equals(orderBy)) {
-            if ("ASC".equalsIgnoreCase(direction)) {
-                query.orderBy(qInterest.name.asc(), qInterest.createdAt.asc());
-            } else {
-                query.orderBy(qInterest.name.desc(), qInterest.createdAt.desc());
-            }
+            return isAsc
+                    ? new OrderSpecifier[]{qInterest.name.asc(), qInterest.createdAt.asc()}
+                    : new OrderSpecifier[]{qInterest.name.desc(), qInterest.createdAt.desc()};
         } else if ("subscriberCount".equals(orderBy)) {
-            if ("ASC".equalsIgnoreCase(direction)) {
-                query.orderBy(qSubscription.id.count().asc(), qInterest.createdAt.asc());
-            } else {
-                query.orderBy(qSubscription.id.count().desc(), qInterest.createdAt.desc());
-            }
+            return isAsc
+                    ? new OrderSpecifier[]{qSubscription.id.countDistinct().asc(), qInterest.createdAt.asc()}
+                    : new OrderSpecifier[]{qSubscription.id.countDistinct().desc(), qInterest.createdAt.desc()};
         }
 
-        query.limit(limit + 1);
-        return query.fetch();
+        return new OrderSpecifier[]{qInterest.createdAt.desc()};
     }
 }
