2ef4a7b4568b307c7fc3b684e8e333b85fbbcddf
seongman Joe <smjoe0302@naver.com>
2026-01-22 18:09:36 +0900

    refactor: 유저 삭제시 댓글 좋아요의 유저ID set null 설정 및 알림 CASCADE 설정 추가

--- src/main/java/com/example/monew/domain/comment/entity/CommentLikes.java
+++ src/main/java/com/example/monew/domain/comment/entity/CommentLikes.java
@@ -22,6 +22,7 @@ public class CommentLikes extends BaseCreatableEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.SET_NULL)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
